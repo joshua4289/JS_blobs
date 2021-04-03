adb21b928b26aed35b3ecb5168139fce8aead1d2
joshlobo4289 <joshua.jude.lobo@gmail.com>
2021-04-03 13:53:14 +0100

    changing enumerable helps in property hiding

--- object_properties.js
+++ object_properties.js
@@ -6,5 +6,14 @@
 
   Object.defineProperty(person,"first",{writable:false});
   Object.freeze(person.first);
+ 
   person.first ="Kris";
+
+  //making objects non-enuberable 
+  // this will hide properties from a for in loop 
+  // OR JSON.stringify
+  // OR Object.keys() 
+
+  Object.defineProperty(person,"age",{enumerable:false});
+  JSON.stringify(person);
   
