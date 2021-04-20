d7c6f0c5e07b7713d0b252300174149ff21d2f7f
= <josh@joshspectre.com>
2021-04-20 07:59:21 +0100

    brackets to destructure arrays

---
+++ destructure_objects.js
@@ -0,0 +1,14 @@
+
+let car = {id:5000,style:'convertible'};
+
+let {id,style}= car;
+console.log(id,style);
+
+//doing de-structure later 
+// the compiler does not know if you are starting a 
+// block or not 
+
+({id,style} = car);
+
+// note we are putting the brakets over the whole 
+// expression 
